8ecefc09566e44a2728f24b4fe6809de1803ac0a
Alex Castro <ac1950@georgetown.edu>
2020-03-23 16:40:56 -0500

    configured basic function with test

---
+++ my_script.py
@@ -0,0 +1,11 @@
+
+
+
+def enlarge(i):
+    return i * 100
+
+
+if __name__ == "__main__":
+    my_number = float(input("Please input a number: "))
+    n = enlarge(my_number)
+    print("ENLARGING THE NUMBER", n) 
